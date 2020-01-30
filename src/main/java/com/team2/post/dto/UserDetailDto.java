240842051bd462f2ae3c025a92577ef7a48ebb32
iAshishChauhan <iashishsinghchauhan@gmail.com>
2020-01-30 15:15:36 +0530

    commit

--- src/main/java/com/team2/post/dto/UserDetailDto.java
+++ src/main/java/com/team2/post/dto/UserDetailDto.java
@@ -1,20 +1,5 @@
 package com.team2.post.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.Date;
-import java.util.List;
-
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class UserDetailDto
-{
 import lombok.*;
 
 import java.util.Date;
